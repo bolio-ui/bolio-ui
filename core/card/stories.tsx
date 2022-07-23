6ea7c1522445690f7436dbe292217861118145cc
Bruno Andrade <brunnoandrade@MacBook-Pro-de-Bruno.local>
2022-07-23 16:53:21 -0300

    update stories card

---
+++ core/card/stories.tsx
@@ -0,0 +1,13 @@
+import { Story, Meta } from '@storybook/react'
+import Card from '.'
+
+export default {
+  title: 'Surface/Card',
+  component: Card
+} as Meta
+
+export const Default: Story = () => (
+  <Card>
+    <p>A basic card.</p>
+  </Card>
+)
