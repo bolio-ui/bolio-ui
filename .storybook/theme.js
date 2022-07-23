08fe1f31e29a976835f162d6413aa3765163505c
Bruno Andrade <brunnoandrade@MacBook-Pro-de-Bruno.local>
2022-07-23 17:52:26 -0300

    update config theme storybook

---
+++ .storybook/theme.js
@@ -0,0 +1,24 @@
+import { create } from '@storybook/theming/create'
+
+export default create({
+  base: 'light',
+
+  colorSecondary: '#C25FFF',
+
+  appBg: '#F8F8F8',
+  appBorderColor: '#EDEDED',
+  appBorderRadius: 6,
+
+  barTextColor: '#999999',
+  barSelectedColor: '#C25FFF',
+  barBg: '#F2F2F2',
+
+  inputBg: 'white',
+  inputBorder: 'rgba(0,0,0,.1)',
+  inputTextColor: '#333333',
+  inputBorderRadius: 4,
+
+  brandTitle: 'Bolio UI',
+  brandImage: '',
+  brandUrl: 'https://bolio-ui.com.br/'
+})
