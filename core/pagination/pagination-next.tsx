71cadf08ccd84348c38315d54c5cf3883807dfe2
Bruno Andrade <brunnoandrade@MacBook-Pro-de-Bruno.local>
2022-07-27 18:34:00 -0300

    add pagination core and stories

---
+++ core/pagination/pagination-next.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import PaginationItem from './pagination-item'
+import { usePaginationContext } from './pagination-context'
+
+export type PaginationNextProps = React.ButtonHTMLAttributes<any>
+
+function PaginationNext({ children, ...props }: PaginationNextProps) {
+  const { update, isLast } = usePaginationContext()
+  return (
+    <PaginationItem
+      onClick={() => update && update('next')}
+      disabled={isLast}
+      {...props}
+    >
+      {children}
+    </PaginationItem>
+  )
+}
+
+PaginationNext.displayName = 'BolioUIPaginationNext'
+export default PaginationNext
