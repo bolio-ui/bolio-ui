cc6543ec50deae2d4dad70c86ec015437c26f992
Bruno Andrade <brunnoandrade@MacBook-Pro-de-Bruno.local>
2022-07-28 21:50:36 -0300

    add button, dropdown and group with stories

---
+++ core/button-dropdown/button-dropdown-context.ts
@@ -0,0 +1,22 @@
+import React from 'react'
+import { NormalTypes } from '../utils/prop-types'
+
+export interface ButtonDropdownConfig {
+  type?: NormalTypes
+  auto?: boolean
+  disabled?: boolean
+  loading?: boolean
+}
+
+const defaultContext = {
+  type: 'default' as NormalTypes,
+  auto: false,
+  disabled: false,
+  loading: false
+}
+
+export const ButtonDropdownContext =
+  React.createContext<ButtonDropdownConfig>(defaultContext)
+
+export const useButtonDropdown = (): ButtonDropdownConfig =>
+  React.useContext<ButtonDropdownConfig>(ButtonDropdownContext)
