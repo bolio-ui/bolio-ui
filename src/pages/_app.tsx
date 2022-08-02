d226f4a6268d07f589ef3e886ba07690345bb4eb
Bruno Andrade <brunnoandrade@MacBook-Pro-de-Bruno.local>
2022-08-02 19:06:19 -0300

    add favicon page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,7 @@ import Head from 'next/head'
 import { BolioUIProvider, CssBaseline } from 'core'
 import { PrefersContext, themes, ThemeType } from 'src/utils/use-prefers'
 import Menu from 'src/components/Navigation/menu'
+import Favicon from 'src/components/Favicon'
 
 function App({ Component, pageProps }: AppProps) {
   const [themeType, setThemeType] = useState<ThemeType>('dark')
@@ -26,13 +27,11 @@ function App({ Component, pageProps }: AppProps) {
     <>
       <Head>
         <title>Bolio UI</title>
-        <link rel="shortcut icon" href="/img/icon-512.png" />
-        <link rel="apple-touch-icon" href="/img/icon-512.png" />
-        <link rel="manifest" href="/manifest.json" />
         <meta
           name="description"
           content="Make your development more creative and dynamic with amazing tools for React."
         />
+        <Favicon />
       </Head>
       <BolioUIProvider themeType={themeType}>
         <PrefersContext.Provider value={{ themeType, switchTheme }}>
