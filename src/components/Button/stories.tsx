401f1808acac8c6d0ba15a5229e5b8ff84070908
Bruno Andrade <brunnoandrade@MacBook-Pro-de-Bruno.local>
2022-08-02 19:09:06 -0300

    remove files

--- src/components/Button/stories.tsx
+++
@@ -1,9 +0,0 @@
-import { Story, Meta } from '@storybook/react'
-import Button from '.'
-
-export default {
-  title: 'Button',
-  component: Button
-} as Meta
-
-export const Default: Story = () => <Button />
