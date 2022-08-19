1bd94fa4b8a1ae14d915d8d15c4d768c31a11bbb
Bruno Andrade <brunnoandrade@MacBook-Pro-de-Bruno.local>
2022-08-18 21:12:28 -0300

    update mdx render pages

--- next.config.js
+++ next.config.js
@@ -8,27 +8,7 @@ const withMDX = require('@next/mdx')({
 const isProd = process.env.NODE_ENV === 'production'
 
 const nextConfig = {
-  generateEtags: false,
-
-  poweredByHeader: false,
-
-  eslint: {
-    ignoreDuringBuilds: true
-  },
-
   pageExtensions: ['jsx', 'js', 'mdx', 'md', 'ts', 'tsx'],
-
-  cssModules: true,
-
-  cssLoaderOptions: {
-    importLoaders: 1,
-    localIdentName: '[local]___[hash:base64:5]'
-  },
-
-  env: {
-    VERSION: require('./package.json').version
-  },
-
   pwa: {
     dest: 'public',
     disable: !isProd
