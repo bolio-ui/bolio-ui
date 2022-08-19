739afc3ba23959ad86aeb7f62ba319c2887c1694
Bruno Andrade <brunnoandrade@MacBook-Pro-de-Bruno.local>
2022-08-19 20:17:20 -0300

    add usemediaquery hook

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,5 @@
 export { default as Playground } from './Playground'
+export { default as PlaygroundTitle } from './Playground/title'
 export { default as Attributes } from './Attributes'
 export { default as AttributesTable } from './Attributes/attributes-table'
 export { default as AttributesTitle } from './Attributes/attributes-title'
