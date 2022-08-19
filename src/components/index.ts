d917ea867964db75946dc702126902feddc9dfe1
Bruno Andrade <brunnoandrade@MacBook-Pro-de-Bruno.local>
2022-08-18 21:10:11 -0300

    add mdx render pages

---
+++ src/components/index.ts
@@ -0,0 +1,4 @@
+export { default as Playground } from './Playground'
+export { default as Attributes } from './Attributes'
+export { default as AttributesTable } from './Attributes/attributes-table'
+export { default as AttributesTitle } from './Attributes/attributes-title'
