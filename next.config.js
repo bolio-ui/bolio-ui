0234c86fc7a27029ad3a59ca5a097c5a4a5baac1
Bruno Andrade <brunnoandrade@MacBook-Pro-de-Bruno.local>
2022-08-20 16:04:44 -0300

    add rehype-join-line and @mapbox/rehype-prism

--- next.config.js
+++ next.config.js
@@ -2,7 +2,13 @@
 const withPWA = require('next-pwa')
 
 const withMDX = require('@next/mdx')({
-  extension: /\.(md|mdx)?$/
+  extension: /\.(md|mdx)?$/,
+  options: {
+    rehypePlugins: [
+      require('@mapbox/rehype-prism'),
+      require('rehype-join-line')
+    ]
+  }
 })
 
 const isProd = process.env.NODE_ENV === 'production'
