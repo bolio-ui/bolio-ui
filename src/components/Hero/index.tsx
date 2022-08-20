762d9a9d4458c2ef4358b1e9c4697396a582983a
Bruno Andrade <brunnoandrade@MacBook-Pro-de-Bruno.local>
2022-08-20 11:31:57 -0300

    update hero page

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import { Text, Container, Grid, Row, Col, Section } from 'core'
+import NextLink from 'next/link'
+import { Text, Container, Grid, Row, Col, Section, Button, Snippet } from 'core'
 
 interface Props {
   content: { title: string; description: string }
@@ -15,9 +16,21 @@ function Hero({ content }: Props) {
           <Row justify="space-around" style={{ textAlign: 'center' }}>
             <Col span={18}>
               <Text h1>{content.title}</Text>
-              <Text p font={1.5}>
+              <Text p font={1.5} mt={0}>
                 {content.description}
               </Text>
+            </Col>
+          </Row>
+          <Row justify="space-around">
+            <Col>
+              <NextLink href="/guide/getting-started" passHref>
+                <Button type="info" auto mr={1} mt={1.5}>
+                  Get Started
+                </Button>
+              </NextLink>
+            </Col>
+            <Col>
+              <Snippet text="yarn add @bolio-ui/core" mt={1.5} />
             </Col>
           </Row>
         </Grid.Container>
