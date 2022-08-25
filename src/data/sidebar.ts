114859013cba2486a7a2ee830bfcf809079aa0d1
Bruno Andrade <brunnoandrade@MacBook-Pro-de-Bruno.local>
2022-08-25 20:47:45 -0300

    add image mdx

--- src/data/sidebar.ts
+++ src/data/sidebar.ts
@@ -100,7 +100,11 @@ export const components = [
   },
   {
     name: 'Icons',
-    url: '/docs/components/icons'
+    url: '/docs/components/icon'
+  },
+  {
+    name: 'Image',
+    url: '/docs/components/image'
   }
 ]
 
