b7fd8b8f771f521a7b625d3935cacab7f47a3f15
Bruno Andrade <brunnoandrade@MacBook-Pro-de-Bruno.local>
2022-09-17 12:25:22 -0300

    add generate package npm scripts

---
+++ scripts/rollup.config.js
@@ -0,0 +1,126 @@
+import commonjs from '@rollup/plugin-commonjs'
+import nodeResolve from '@rollup/plugin-node-resolve'
+import localResolve from 'rollup-plugin-local-resolve'
+import babel from 'rollup-plugin-babel'
+import fs from 'fs-extra'
+import path from 'path'
+
+const root = path.join(__dirname, '../')
+const corePath = path.join(root, 'core')
+const distPath = path.join(root, 'dist')
+const esmPath = path.join(root, 'esm')
+
+const extensions = ['.js', '.jsx', '.ts', '.tsx']
+
+const plugins = [
+  babel({
+    exclude: 'node_modules/**',
+    extensions,
+    presets: [
+      '@babel/preset-env',
+      '@babel/preset-react',
+      '@babel/preset-typescript'
+    ],
+    plugins: ['styled-jsx/babel'],
+    babelrc: false,
+    sourcemap: false
+  }),
+  localResolve(),
+  nodeResolve({
+    browser: true,
+    extensions
+  }),
+  commonjs()
+]
+
+const globals = {
+  react: 'React',
+  'react-dom': 'ReactDOM'
+}
+
+const external = (id) => /^react|react-dom|next\/link/.test(id)
+
+const cjsOutput = {
+  format: 'cjs',
+  exports: 'named',
+  entryFileNames: '[name]/index.js',
+  dir: distPath,
+  manualChunks: (id) => {
+    if (id.includes('node_modules/styled-jsx')) {
+      return 'styled-jsx.cjs'
+    }
+  },
+  chunkFileNames: '[name].js',
+  globals,
+  sourcemap: false
+}
+
+const esmOutput = {
+  format: 'es',
+  entryFileNames: '[name]/index.js',
+  dir: esmPath,
+  manualChunks: (id) => {
+    if (id.includes('node_modules/styled-jsx/server')) {
+      return 'styled-jsx-server.es'
+    }
+    if (id.includes('node_modules/styled-jsx')) {
+      return 'styled-jsx.es'
+    }
+  },
+  chunkFileNames: '[name].js',
+  globals
+}
+
+export default (async () => {
+  await fs.remove(distPath)
+  await fs.remove(esmPath)
+  const files = await fs.readdir(corePath)
+
+  const core = await Promise.all(
+    files.map(async (name) => {
+      const unitPath = path.join(corePath, name)
+      const entry = path.join(unitPath, 'index.ts')
+
+      const stat = await fs.stat(unitPath)
+      if (!stat.isDirectory()) return null
+
+      const hasFile = await fs.pathExists(entry)
+      if (!hasFile) return null
+
+      return { name, url: entry }
+    })
+  )
+  console.log(
+    `\n${Object.keys(core).length} Components in total have been collected.`
+  )
+
+  return [
+    // Bundle each component separately
+    ...core
+      .filter((r) => !!r)
+      .map(({ name, url }) => ({
+        input: { [name]: url },
+        // output: [esmOutput, cjsOutput],
+        output: [cjsOutput],
+        external,
+        plugins
+      })),
+    // Bundle for packages containing all components.
+    {
+      input: { index: path.join(corePath, 'index.ts') },
+      output: [
+        // {
+        //   ...esmOutput,
+        //
+        //   entryFileNames: 'index.js',
+        // },
+        {
+          ...cjsOutput,
+          entryFileNames: 'index.js'
+        }
+      ],
+      external,
+      plugins
+    }
+  ]
+})()
