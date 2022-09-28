d20411cd6b0651714fcbd4d2633084eb59dc3ad2
Bruno Andrade <brunnoandradi@gmail.com>
2022-09-28 20:16:46 -0300

    update docs

--- core/button-dropdown/stories.tsx
+++ core/button-dropdown/stories.tsx
@@ -1,6 +1,6 @@
 import { Story, Meta } from '@storybook/react'
 import ButtonDropdown from '.'
-import Spacer from '../spacer'
+import Grid from '../grid'
 import { Plus } from '@bolio-ui/icons'
 
 export default {
@@ -9,63 +9,88 @@ export default {
 } as Meta
 
 export const Default: Story = () => (
-  <ButtonDropdown>
-    <ButtonDropdown.Item main>Default Button</ButtonDropdown.Item>
-    <ButtonDropdown.Item>Secondary Button</ButtonDropdown.Item>
-    <ButtonDropdown.Item>Tertiary Button</ButtonDropdown.Item>
-  </ButtonDropdown>
+  <Grid.Container gap={2}>
+    <Grid>
+      <ButtonDropdown>
+        <ButtonDropdown.Item main>Default Button</ButtonDropdown.Item>
+        <ButtonDropdown.Item>Secondary Button</ButtonDropdown.Item>
+        <ButtonDropdown.Item>Tertiary Button</ButtonDropdown.Item>
+      </ButtonDropdown>
+    </Grid>
+  </Grid.Container>
 )
 
 export const Loading: Story = () => (
-  <ButtonDropdown loading>
-    <ButtonDropdown.Item main>Default Button</ButtonDropdown.Item>
-    <ButtonDropdown.Item>Secondary Button</ButtonDropdown.Item>
-    <ButtonDropdown.Item>Tertiary Button</ButtonDropdown.Item>
-  </ButtonDropdown>
+  <Grid.Container gap={2}>
+    <Grid>
+      <ButtonDropdown loading>
+        <ButtonDropdown.Item main>Default Button</ButtonDropdown.Item>
+        <ButtonDropdown.Item>Secondary Button</ButtonDropdown.Item>
+        <ButtonDropdown.Item>Tertiary Button</ButtonDropdown.Item>
+      </ButtonDropdown>
+    </Grid>
+  </Grid.Container>
 )
 
 export const Disabled: Story = () => (
-  <ButtonDropdown disabled>
-    <ButtonDropdown.Item main>Default Button</ButtonDropdown.Item>
-    <ButtonDropdown.Item>Secondary Button</ButtonDropdown.Item>
-    <ButtonDropdown.Item>Tertiary Button</ButtonDropdown.Item>
-  </ButtonDropdown>
+  <Grid.Container gap={2}>
+    <Grid>
+      <ButtonDropdown disabled>
+        <ButtonDropdown.Item main>Default Button</ButtonDropdown.Item>
+        <ButtonDropdown.Item>Secondary Button</ButtonDropdown.Item>
+        <ButtonDropdown.Item>Tertiary Button</ButtonDropdown.Item>
+      </ButtonDropdown>
+    </Grid>
+  </Grid.Container>
 )
 
 export const Types: Story = () => (
-  <>
-    <ButtonDropdown type="secondary" scale={0.5}>
-      <ButtonDropdown.Item main>Secondary Button</ButtonDropdown.Item>
-    </ButtonDropdown>
-    <Spacer h={0.5} />
-    <ButtonDropdown type="success" scale={0.5}>
-      <ButtonDropdown.Item main>Success Button</ButtonDropdown.Item>
-    </ButtonDropdown>
-    <Spacer h={0.5} />
-    <ButtonDropdown type="warning" scale={0.5}>
-      <ButtonDropdown.Item main>Warning Button</ButtonDropdown.Item>
-    </ButtonDropdown>
-    <Spacer h={0.5} />
-    <ButtonDropdown type="error" scale={0.5}>
-      <ButtonDropdown.Item main>Error Button</ButtonDropdown.Item>
-    </ButtonDropdown>
-  </>
+  <Grid.Container gap={2}>
+    <Grid>
+      <ButtonDropdown type="secondary" scale={0.5}>
+        <ButtonDropdown.Item main>Secondary Button</ButtonDropdown.Item>
+      </ButtonDropdown>
+    </Grid>
+    <Grid>
+      <ButtonDropdown type="success" scale={0.5}>
+        <ButtonDropdown.Item main>Success Button</ButtonDropdown.Item>
+      </ButtonDropdown>
+    </Grid>
+    <Grid>
+      <ButtonDropdown type="warning" scale={0.5}>
+        <ButtonDropdown.Item main>Warning Button</ButtonDropdown.Item>
+      </ButtonDropdown>
+    </Grid>
+    <Grid>
+      <ButtonDropdown type="error" scale={0.5}>
+        <ButtonDropdown.Item main>Error Button</ButtonDropdown.Item>
+      </ButtonDropdown>
+    </Grid>
+  </Grid.Container>
 )
 
 export const MultipleType: Story = () => (
-  <ButtonDropdown scale={2 / 3} auto>
-    <ButtonDropdown.Item main>Check</ButtonDropdown.Item>
-    <ButtonDropdown.Item>Block</ButtonDropdown.Item>
-    <ButtonDropdown.Item>Lock</ButtonDropdown.Item>
-    <ButtonDropdown.Item type="error">Destroy</ButtonDropdown.Item>
-  </ButtonDropdown>
+  <Grid.Container gap={2}>
+    <Grid>
+      <ButtonDropdown scale={2 / 3} auto>
+        <ButtonDropdown.Item main>Check</ButtonDropdown.Item>
+        <ButtonDropdown.Item>Block</ButtonDropdown.Item>
+        <ButtonDropdown.Item>Lock</ButtonDropdown.Item>
+        <ButtonDropdown.Item type="error">Destroy</ButtonDropdown.Item>
+      </ButtonDropdown>
+    </Grid>
+  </Grid.Container>
 )
 
 export const CustomIcon: Story = () => (
-  <ButtonDropdown scale={2 / 3} auto icon={<Plus />}>
-    <ButtonDropdown.Item main>Check User</ButtonDropdown.Item>
-    <ButtonDropdown.Item>Block User</ButtonDropdown.Item>
-    <ButtonDropdown.Item>Lock User</ButtonDropdown.Item>
-    <ButtonDropdown.Item type="error">Destroy User</ButtonDropdown.Item>
-  </ButtonDropdown>
+  <Grid.Container gap={2}>
+    <Grid>
+      <ButtonDropdown scale={2 / 3} auto icon={<Plus />}>
+        <ButtonDropdown.Item main>Check User</ButtonDropdown.Item>
+        <ButtonDropdown.Item>Block User</ButtonDropdown.Item>
+        <ButtonDropdown.Item>Lock User</ButtonDropdown.Item>
+        <ButtonDropdown.Item type="error">Destroy User</ButtonDropdown.Item>
+      </ButtonDropdown>
+    </Grid>
+  </Grid.Container>
 )
