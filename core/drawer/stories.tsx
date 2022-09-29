048c5457ca0925be10aed2a44bac5e4de5c053ab
Bruno Andrade <brunnoandradi@gmail.com>
2022-09-29 14:05:51 -0300

    update dot, drawer, fieldset and icons docs

--- core/drawer/stories.tsx
+++ core/drawer/stories.tsx
@@ -3,6 +3,7 @@ import React, { useState } from 'react'
 import { DrawerPlacement } from './helper'
 import Drawer from '.'
 import Button from '../button'
+import Grid from '../grid'
 
 export default {
   title: 'Feedback/Drawer',
@@ -12,42 +13,56 @@ export default {
 export const Default: Story = () => {
   const [state, setState] = useState(false)
   return (
-    <div>
-      <Button auto onClick={() => setState(true)} scale={1 / 2}>
-        Show Drawer
-      </Button>
-      <Drawer visible={state} onClose={() => setState(false)} placement="right">
-        <Drawer.Title>Drawer</Drawer.Title>
-        <Drawer.Subtitle>Example of a drawer</Drawer.Subtitle>
-        <Drawer.Content>
-          <p>Demo content for the drawer.</p>
-        </Drawer.Content>
-      </Drawer>
-    </div>
+    <Grid.Container gap={2}>
+      <Grid>
+        <Button auto onClick={() => setState(true)}>
+          Show Drawer
+        </Button>
+        <Drawer
+          visible={state}
+          onClose={() => setState(false)}
+          placement="right"
+        >
+          <Drawer.Title>Drawer</Drawer.Title>
+          <Drawer.Subtitle>Example of a drawer</Drawer.Subtitle>
+          <Drawer.Content>
+            <p>Demo content for the drawer.</p>
+          </Drawer.Content>
+        </Drawer>
+      </Grid>
+    </Grid.Container>
   )
 }
 
 export const Placement: Story = () => {
   const [state, setState] = React.useState(false)
   const [placement, setPlacement] = React.useState<DrawerPlacement>('bottom')
-  const open = (text: any) => {
+  const open = (text) => {
     setPlacement(text)
     setState(true)
   }
   return (
-    <div>
-      <Button auto onClick={() => open('top')} scale={1 / 2} mr="10px">
-        Top
-      </Button>
-      <Button auto onClick={() => open('right')} scale={1 / 2} mr="10px">
-        Right
-      </Button>
-      <Button auto onClick={() => open('bottom')} scale={1 / 2} mr="10px">
-        Bottom
-      </Button>
-      <Button auto onClick={() => open('left')} scale={1 / 2}>
-        Left
-      </Button>
+    <Grid.Container gap={2}>
+      <Grid>
+        <Button auto onClick={() => open('top')} mr="10px">
+          Top
+        </Button>
+      </Grid>
+      <Grid>
+        <Button auto onClick={() => open('right')} mr="10px">
+          Right
+        </Button>
+      </Grid>
+      <Grid>
+        <Button auto onClick={() => open('bottom')} mr="10px">
+          Bottom
+        </Button>
+      </Grid>
+      <Grid>
+        <Button auto onClick={() => open('left')}>
+          Left
+        </Button>
+      </Grid>
       <Drawer
         visible={state}
         onClose={() => setState(false)}
@@ -59,6 +74,6 @@ export const Placement: Story = () => {
           <p>Demo content for the drawer.</p>
         </Drawer.Content>
       </Drawer>
-    </div>
+    </Grid.Container>
   )
 }
