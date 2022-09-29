6074d2ac621f2611c12c5b9a5d130605e75f12d9
Bruno Andrade <brunnoandradi@gmail.com>
2022-09-29 11:38:04 -0300

    update code, collapse and description docs

--- core/code/stories.tsx
+++ core/code/stories.tsx
@@ -1,6 +1,7 @@
 import { Story, Meta } from '@storybook/react'
 import Code from '.'
 import Text from '../text'
+import Grid from '../grid'
 import WaitTime from '../mdx-widgets/parsed-codes/wait-time'
 
 export default {
@@ -9,36 +10,54 @@ export default {
 } as Meta
 
 export const Default: Story = () => (
-  <>
-    <Text my={0}>
-      Run <Code>npm i @bolio-ui/core</Code> to install.
-    </Text>
-    <Text my={0}>
-      Or run <Code>yarn add @bolio-ui/core</Code> to install.
-    </Text>
-  </>
+  <Grid.Container gap={2}>
+    <Grid>
+      <Text my={0}>
+        Run <Code>npm i @bolio-ui/core</Code> to install.
+      </Text>
+      <Text my={0}>
+        Or run <Code>yarn add @bolio-ui/core</Code> to install.
+      </Text>
+    </Grid>
+  </Grid.Container>
 )
 
 export const Block: Story = () => (
-  <Code block my={0}>
-    <WaitTime />
-  </Code>
+  <Grid.Container gap={2}>
+    <Grid>
+      <Code block my={0}>
+        <WaitTime />
+      </Code>
+    </Grid>
+  </Grid.Container>
 )
 
 export const Width: Story = () => (
-  <Code block width="50%" my={0}>
-    <WaitTime />
-  </Code>
+  <Grid.Container gap={2}>
+    <Grid>
+      <Code block width="50%" my={0}>
+        <WaitTime />
+      </Code>
+    </Grid>
+  </Grid.Container>
 )
 
 export const Name: Story = () => (
-  <Code block name="/Components/WaitTime.jsx" my={0}>
-    <WaitTime />
-  </Code>
+  <Grid.Container gap={2}>
+    <Grid>
+      <Code block name="/Components/WaitTime.jsx" my={0}>
+        <WaitTime />
+      </Code>
+    </Grid>
+  </Grid.Container>
 )
 
 export const Classic: Story = () => (
-  <Code block classic name="/Components/WaitTime.jsx" my={0}>
-    <WaitTime />
-  </Code>
+  <Grid.Container gap={2}>
+    <Grid>
+      <Code block classic name="/Components/WaitTime.jsx" my={0}>
+        <WaitTime />
+      </Code>
+    </Grid>
+  </Grid.Container>
 )
