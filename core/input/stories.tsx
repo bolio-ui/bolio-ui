f2cc1d37ce2b65afc3962b54e7175a522078a486
Bruno Andrade <brunnoandradi@gmail.com>
2022-09-29 14:32:17 -0300

    update input and image docs

--- core/input/stories.tsx
+++ core/input/stories.tsx
@@ -1,6 +1,6 @@
 import { Story, Meta } from '@storybook/react'
 import Input from '.'
-import Spacer from '../spacer'
+import Grid from '../grid'
 import Text from '../text'
 import { Instagram, Facebook } from '@bolio-ui/icons'
 
@@ -9,61 +9,107 @@ export default {
   component: Input
 } as Meta
 
-export const Default: Story = () => <Input placeholder="Placeholder Text" />
+export const Default: Story = () => (
+  <Grid.Container gap={2}>
+    <Grid>
+      <Input placeholder="Placeholder Text" />
+    </Grid>
+  </Grid.Container>
+)
 
 export const Sizes: Story = () => (
-  <>
-    <Input scale={1 / 3} placeholder="Scale 1/3" /> <Spacer h={0.5} />
-    <Input scale={2 / 3} placeholder="Scale 2/3" /> <Spacer h={0.5} />
-    <Input placeholder="Scale 1" /> <Spacer h={0.5} />
-    <Input scale={4 / 3} placeholder="Scale 4/3" />
-  </>
+  <Grid.Container gap={2}>
+    <Grid>
+      <Input scale={1 / 3} placeholder="Scale 1/3" />
+    </Grid>
+    <Grid>
+      <Input scale={2 / 3} placeholder="Scale 2/3" />
+    </Grid>
+    <Grid>
+      <Input placeholder="Scale 1" />
+    </Grid>
+    <Grid>
+      <Input scale={4 / 3} placeholder="Scale 4/3" />
+    </Grid>
+  </Grid.Container>
 )
 
 export const SetWidth: Story = () => (
-  <Input placeholder="Placeholder Text" width="100%" />
+  <Grid.Container gap={2}>
+    <Grid>
+      <Input placeholder="Placeholder Text" width="100%" />
+    </Grid>
+  </Grid.Container>
 )
 
 export const Unwritable: Story = () => (
-  <>
-    <Input disabled placeholder="Disabled" />
-    <Spacer h={0.5} />
-    <Input readOnly initialValue="Read Only" />
-  </>
+  <Grid.Container gap={2}>
+    <Grid>
+      <Input disabled placeholder="Disabled" />
+    </Grid>
+    <Grid>
+      <Input readOnly initialValue="Read Only" />
+    </Grid>
+  </Grid.Container>
 )
 
 export const BlockLabel: Story = () => (
-  <>
-    <Input placeholder="Block Label" width="50%">
-      <Text h3>Block Label</Text>
-    </Input>
-    <Spacer />
-    <Input placeholder="Block Label">Block Label</Input>
-  </>
+  <Grid.Container gap={2}>
+    <Grid>
+      <Input placeholder="Block Label">
+        <Text h3>Block Label</Text>
+      </Input>
+    </Grid>
+    <Grid>
+      <Input placeholder="Block Label">Block Label</Input>
+    </Grid>
+  </Grid.Container>
 )
 
 export const Type: Story = () => (
-  <>
-    <Input type="secondary" initialValue="Initial Value" />
-    <Spacer h={0.5} />
-    <Input type="success" initialValue="Initial Value" />
-    <Spacer h={0.5} />
-    <Input type="warning" initialValue="Initial Value" />
-    <Spacer h={0.5} />
-    <Input type="error" initialValue="Initial Value" />
-  </>
+  <Grid.Container gap={2}>
+    <Grid>
+      <Input type="secondary" initialValue="Initial Value" />
+    </Grid>
+    <Grid>
+      <Input type="success" initialValue="Initial Value" />
+    </Grid>
+    <Grid>
+      <Input type="warning" initialValue="Initial Value" />
+    </Grid>
+    <Grid>
+      <Input type="error" initialValue="Initial Value" />
+    </Grid>
+  </Grid.Container>
 )
 
 export const Icon: Story = () => (
-  <>
-    <Input icon={<Instagram />} placeholder="Instagram" />
-    <Spacer h={0.5} />
-    <Input iconRight={<Facebook />} placeholder="Facebook" />
-  </>
+  <Grid.Container gap={2}>
+    <Grid>
+      <Input icon={<Instagram />} placeholder="Instagram" />
+    </Grid>
+    <Grid>
+      <Input iconRight={<Facebook />} placeholder="Facebook" />
+    </Grid>
+  </Grid.Container>
 )
 
 export const Clearable: Story = () => (
-  <Input clearable initialValue="Initial Value" placeholder="Initial Value" />
+  <Grid.Container gap={2}>
+    <Grid>
+      <Input
+        clearable
+        initialValue="Initial Value"
+        placeholder="Initial Value"
+      />
+    </Grid>
+  </Grid.Container>
 )
 
-export const Password: Story = () => <Input.Password initialValue="123456abc" />
+export const Password: Story = () => (
+  <Grid.Container gap={2}>
+    <Grid>
+      <Input.Password initialValue="123456abc" />
+    </Grid>
+  </Grid.Container>
+)
