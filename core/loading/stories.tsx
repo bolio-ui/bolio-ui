11b3966fa5da4ad08beaf5576ce782867028058d
Bruno Andrade <brunnoandradi@gmail.com>
2022-09-29 15:57:26 -0300

    update loading, modal, note and page docs

--- core/loading/stories.tsx
+++ core/loading/stories.tsx
@@ -7,34 +7,46 @@ export default {
   component: Loading
 } as Meta
 
-export const Default: Story = () => <Loading />
+export const Default: Story = () => (
+  <Grid.Container gap={2}>
+    <Grid xs={12} md={12}>
+      <Loading />
+    </Grid>
+  </Grid.Container>
+)
 
-export const WithText: Story = () => <Loading>Loading</Loading>
+export const WithText: Story = () => (
+  <Grid.Container gap={2}>
+    <Grid xs={12} md={12}>
+      <Loading>Loading</Loading>
+    </Grid>
+  </Grid.Container>
+)
 
 export const Types: Story = () => (
-  <Grid.Container gap={2.5}>
-    <Grid xs={12}>
+  <Grid.Container gap={2}>
+    <Grid xs={3} md={3}>
       <Loading type="success" />
     </Grid>
-    <Grid xs={12}>
+    <Grid xs={3} md={3}>
       <Loading type="secondary" />
     </Grid>
-    <Grid xs={12}>
+    <Grid xs={3} md={3}>
       <Loading type="warning" />
     </Grid>
-    <Grid xs={12}>
+    <Grid xs={3} md={3}>
       <Loading type="error" />
     </Grid>
   </Grid.Container>
 )
 
 export const Spaces: Story = () => (
-  <Grid.Container gap={2.5}>
-    <Grid xs={12}>
-      <Loading spaceRatio={2.5} />
+  <Grid.Container gap={2}>
+    <Grid xs={6} md={6}>
+      <Loading spaceRatio={10} />
     </Grid>
-    <Grid xs={12}>
-      <Loading spaceRatio={0.5} />
+    <Grid xs={6} md={6}>
+      <Loading spaceRatio={5} />
     </Grid>
   </Grid.Container>
 )
