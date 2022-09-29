1e9fc8ea9b6887232578d9b0fb804ddc2816678b
Bruno Andrade <brunnoandradi@gmail.com>
2022-09-28 21:05:49 -0300

    update checkbox, card and capacity docs

--- core/checkbox/stories.tsx
+++ core/checkbox/stories.tsx
@@ -1,6 +1,6 @@
 import { Story, Meta } from '@storybook/react'
 import Checkbox from '.'
-import Spacer from '../spacer'
+import Grid from '../grid'
 
 export default {
   title: 'Data Entry/Checkbox',
@@ -8,63 +8,85 @@ export default {
 } as Meta
 
 export const Default: Story = () => (
-  <Checkbox checked={true}>Check option</Checkbox>
+  <Grid.Container gap={2}>
+    <Grid>
+      <Checkbox checked={true}>Check option</Checkbox>
+    </Grid>
+  </Grid.Container>
 )
 
 export const Scale: Story = () => (
-  <>
-    <Checkbox mr="15px" checked={true} scale={0.25}>
-      scale-0.25
-    </Checkbox>
-    <Checkbox mr="15px" checked={true} scale={0.5}>
-      scale-0.5
-    </Checkbox>
-    <Checkbox mr="15px" checked={true} scale={0.75}>
-      scale-0.75
-    </Checkbox>
-  </>
+  <Grid.Container gap={2}>
+    <Grid>
+      <Checkbox mr="15px" checked={true} scale={0.25}>
+        scale-0.25
+      </Checkbox>
+    </Grid>
+    <Grid>
+      <Checkbox mr="15px" checked={true} scale={0.5}>
+        scale-0.5
+      </Checkbox>
+    </Grid>
+    <Grid>
+      <Checkbox mr="15px" checked={true} scale={0.75}>
+        scale-0.75
+      </Checkbox>
+    </Grid>
+  </Grid.Container>
 )
 
 export const Type: Story = () => (
-  <>
-    <Checkbox checked={true} type="default">
-      Checkbox Default
-    </Checkbox>
-    <Spacer h={0.5} />
-    <Checkbox checked={true} type="success">
-      Checkbox Success
-    </Checkbox>
-    <Spacer h={0.5} />
-    <Checkbox checked={true} type="warning">
-      Checkbox Warning
-    </Checkbox>
-    <Spacer h={0.5} />
-    <Checkbox checked={true} type="error">
-      Checkbox Error
-    </Checkbox>
-  </>
+  <Grid.Container gap={2}>
+    <Grid>
+      <Checkbox checked={true} type="default">
+        Checkbox Default
+      </Checkbox>
+    </Grid>
+    <Grid>
+      <Checkbox checked={true} type="success">
+        Checkbox Success
+      </Checkbox>
+    </Grid>
+    <Grid>
+      <Checkbox checked={true} type="warning">
+        Checkbox Warning
+      </Checkbox>
+    </Grid>
+    <Grid>
+      <Checkbox checked={true} type="error">
+        Checkbox Error
+      </Checkbox>
+    </Grid>
+  </Grid.Container>
 )
 
 export const Disabled: Story = () => (
-  <>
-    <Checkbox>Checkbox option</Checkbox>
-    <Spacer h={0.5} />
-    <Checkbox disabled checked={true}>
-      Checkbox option disabled
-    </Checkbox>
-  </>
+  <Grid.Container gap={2}>
+    <Grid>
+      <Checkbox>Checkbox option</Checkbox>
+    </Grid>
+    <Grid>
+      <Checkbox disabled checked={true}>
+        Checkbox option disabled
+      </Checkbox>
+    </Grid>
+  </Grid.Container>
 )
 
 export const Group: Story = () => {
-  const handler = (value: any) => {
+  const handler = (value) => {
     console.log(value)
   }
   return (
-    <Checkbox.Group value={['check3']} onChange={handler}>
-      <Checkbox value="check1">Checkbox option 1</Checkbox>
-      <Checkbox value="check2">Checkbox option 2</Checkbox>
-      <Checkbox value="check3">Checkbox option 3</Checkbox>
-      <Checkbox value="check4">Checkbox option 4</Checkbox>
-    </Checkbox.Group>
+    <Grid.Container gap={2}>
+      <Grid>
+        <Checkbox.Group value={['check3']} onChange={handler}>
+          <Checkbox value="check1">Checkbox option 1</Checkbox>
+          <Checkbox value="check2">Checkbox option 2</Checkbox>
+          <Checkbox value="check3">Checkbox option 3</Checkbox>
+          <Checkbox value="check4">Checkbox option 4</Checkbox>
+        </Checkbox.Group>
+      </Grid>
+    </Grid.Container>
   )
 }
