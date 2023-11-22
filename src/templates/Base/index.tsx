d1ad2c259ff5600f8edd406e110d632179275fe9
Bruno Andrade <brunnoandradi@gmail.com>
2023-11-22 19:49:35 -0300

    update gradients bg in template base

--- src/templates/Base/index.tsx
+++ src/templates/Base/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import { Page } from 'core'
+import { Page, Image } from 'core'
+import { useMediaQuery } from 'src/utils/use-media-query'
 import Footer from 'src/components/Footer'
 
 export type BaseTemplateProps = {
@@ -7,12 +8,67 @@ export type BaseTemplateProps = {
 }
 
 function Base({ children }: BaseTemplateProps) {
+  const isMobile = useMediaQuery(650)
+
   return (
     <Page style={{ zIndex: 1 }}>
       <Page.Content>{children}</Page.Content>
       <Page.Footer>
         <Footer />
       </Page.Footer>
+      {isMobile ? (
+        <>
+          <Image
+            src="/img/png/home/hero-bg.png"
+            alt="docs background gradient blue"
+            draggable={false}
+            style={{
+              position: 'fixed',
+              top: '-10%',
+              right: '-35%',
+              zIndex: 0
+            }}
+          />
+          <Image
+            src="/img/png/home/hero-bg.png"
+            alt="docs background gradient violet"
+            draggable={false}
+            style={{
+              position: 'fixed',
+              top: '45%',
+              left: '-35%',
+              zIndex: 0
+            }}
+          />
+        </>
+      ) : (
+        <>
+          <Image
+            src="/img/png/home/hero-bg.png"
+            alt="docs background gradient blue"
+            draggable={false}
+            style={{
+              position: 'fixed',
+              bottom: '-50%',
+              top: '-40%',
+              right: '-10%',
+              zIndex: 0
+            }}
+          />
+          <Image
+            src="/img/png/home/hero-bg.png"
+            alt="docs background gradient violet"
+            draggable={false}
+            style={{
+              position: 'fixed',
+              bottom: '-50%',
+              left: '-20%',
+              right: '-50%',
+              zIndex: 0
+            }}
+          />
+        </>
+      )}
     </Page>
   )
 }
