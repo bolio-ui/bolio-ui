210b83ffb76d7047112d8822db1a56913ab71646
Bruno Andrade <brunnoandradi@gmail.com>
2023-12-09 21:13:33 -0300

    update manifest

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -62,6 +62,11 @@ function App({ Component, pageProps }: AppProps) {
           name="viewport"
           content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover"
         />
+        <link
+          rel="manifest"
+          href="/manifest.json"
+          crossOrigin="use-credentials"
+        />
         <Favicon />
       </Head>
       <BolioUIProvider themeType={themeType}>
