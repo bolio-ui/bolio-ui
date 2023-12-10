9c6a8a2a5900a3cee345bbf1b6b3b907e9663424
Bruno Andrade <brunnoandradi@gmail.com>
2023-12-10 12:51:41 -0300

    update manifest and images favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -62,6 +62,7 @@ function App({ Component, pageProps }: AppProps) {
           name="viewport"
           content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover"
         />
+        <meta name="theme-color" content="#000" />
         <link
           rel="manifest"
           href="/manifest.json"
