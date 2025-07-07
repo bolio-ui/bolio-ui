c41ebf07f8cd37507c4a4ff6a13e994981ddb91c
Bruno Andrade <brunnoandradi@gmail.com>
2025-07-07 20:31:52 -0300

    add configs security in next config

--- next.config.js
+++ next.config.js
@@ -17,7 +17,42 @@ const withPWA = require('next-pwa')({
 })
 
 const nextConfig = {
-  pageExtensions: ['jsx', 'js', 'mdx', 'md', 'ts', 'tsx']
+  pageExtensions: ['jsx', 'js', 'mdx', 'md', 'ts', 'tsx'],
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          { key: 'X-Frame-Options', value: 'SAMEORIGIN' },
+          { key: 'X-Content-Type-Options', value: 'nosniff' },
+          {
+            key: 'Content-Security-Policy',
+            value: `
+              default-src 'self';
+              script-src 'self' 'unsafe-inline' 'unsafe-eval';
+              style-src 'self' 'unsafe-inline';
+              img-src 'self' data: https: blob:;
+              font-src 'self' https:;
+              object-src 'none';
+              base-uri 'self';
+              frame-ancestors 'none';
+            `
+              .replace(/\s{2,}/g, ' ')
+              .trim()
+          },
+          { key: 'Referrer-Policy', value: 'strict-origin-when-cross-origin' },
+          {
+            key: 'Strict-Transport-Security',
+            value: 'max-age=31536000; includeSubDomains; preload'
+          },
+          {
+            key: 'Permissions-Policy',
+            value: 'camera=(), microphone=(), geolocation=()'
+          }
+        ]
+      }
+    ]
+  }
 }
 
 module.exports = withPWA(withMDX(nextConfig))
